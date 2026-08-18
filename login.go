9f1d6168cda67e27efd07f5fc07f31ba759438f2
tarun <tarunpayup@gmail.com>
2026-08-19 02:01:57 +0530

    CORS Changes

--- login.go
+++ login.go
@@ -123,7 +123,23 @@ func StartLoginServer(port string) error {
 
 	fmt.Printf("CAOS Login API running on %s\n", addr)
 
-	return http.ListenAndServe(addr, mux)
+	return http.ListenAndServe(addr, corsMiddleware(mux))
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Max-Age", "600")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 // ============================================================
